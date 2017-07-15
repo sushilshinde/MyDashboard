1fb176b12cbbb44a2306405c1825a50e20da4913
Sushil From Win10 <shinde.sushil@gmail.com>
2017-07-15 21:54:30 +0530

    Initial commit

---
+++ dashboard.js
@@ -0,0 +1,4 @@
+// Materialize.toast(message, displayLength, className, completeCallback);
+$(document).on('click',function(){
+      Materialize.toast('Today Added!', 2000) // 4000 is the duration of the toast
+})
