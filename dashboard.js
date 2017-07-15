3e67de3b4e95d2680e01776fe6845d3c0bd60f6b
Sushil From Mint <shinde.sushil@gmail.com>
2017-07-16 02:46:56 +0530

    Added Bg

--- dashboard.js
+++ dashboard.js
@@ -1,35 +1,46 @@
 // Materialize.toast(message, displayLength, className, completeCallback);
-$(document).on('click', function () {
-      /*
-             chrome.storage.onChanged.addListener(function(changes, namespace) {
-              for (key in changes) {
-                var storageChange = changes[key];
-                console.log('Storage key "%s" in namespace "%s" changed. ' +
-                            'Old value was "%s", new value is "%s".',
-                            key,
-                            namespace,
-                            storageChange.oldValue,
-                            storageChange.newValue);
-              }
+$(function () {
+
+      function loadTodos(){
+             chrome.storage.sync.get(["todoList"], function (todos) {
+                  var td = todos.todoList || [];
+
+                  var str ="";
+
+                  td.forEach(function (element) {
+                        str = str + '<li class="collection-item hoverable" style="background-color: rgba(255, 255, 255, 0.85);">' +
+                              '<div>' + element+ '<a href="#!" class="secondary-content"><i class="material-icons red-text" >delete</i></a>&nbsp;<a style="padding-right:15px" href="#!" class="secondary-content"><i class="material-icons">done</i></a></div>' +
+                              '</li>';
+                  console.log(element);
+
+                  });
+
+                  $("#todo-list").html(str);
+               
             });
-      */
+      }
+
+      loadTodos();
+
+      chrome.storage.onChanged.addListener(function (changes, namespace) {
+           loadTodos();
+      });
+
 
       $("#target").submit(function (event) {
-            Materialize.toast('Today Added!', 2000) // 4000 is the duration of the toast
+            Materialize.toast('Today Added!', 0) // 4000 is the duration of the toast
             var val = $("#todo").val();
+            $("#todo").val("");
+            //TODO : new Date().getTime() as TODO ID
+            chrome.storage.sync.get(["todoList"], function (todos) {
+                  var td = todos.todoList || [];
+                  td.push(val);
+                  //console.log(td);
+                  chrome.storage.sync.set({
+                        'todoList': td
+                  });
 
-            chrome.storage.sync.get("todos", function (todos) {
-                  // Notify that we saved.
-                  message('Settings saved');
-            });
-
-
-            // Save it using the Chrome extension storage API.
-            chrome.storage.sync.set({
-                  'value': val
-            }, function () {
-                  // Notify that we saved.
-                  message('Settings saved');
+                 // console.log(todos);
             });
 
             event.preventDefault();
